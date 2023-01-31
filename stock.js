3d155d2a800f3eb259ee44c8e045b5b85215c8df
fulviagarcia <fulvia21gh@gmail.com>
2023-01-30 23:01:10 -0300

    inicio

---
+++ stock.js
@@ -0,0 +1,15 @@
+let stockProductos = [
+    {id: 1, nombre: "sensorial 1", tipo: "frutas de abrojo", cantidad: 1, desc: "hermoso set para practicar la motricidad fina, la coordinacion viso-motora", precio: 3800, talle: "L", img: './img/frutas(1).jpeg'},
+
+    {id: 2, nombre: "sensorial 2", tipo: "tubo  plegable", cantidad: 1, desc: "Desarrollo de habilidades, ayuda a los niños sensoriales a calmarse", precio: 1980, talle: "L", img: './img/buzo.jpg'},
+
+    {id: 3, nombre: "sensorial 3", tipo: "sobre", cantidad: 1, desc: "Sobre de lectoescritura , estimula y desarrolla motricidad fina", precio: 3900, talle: "M", img: './img/buzo.jpg'},
+
+    {id: 4, nombre: "sensorial 4", tipo: "squishy", cantidad: 1, desc: "ayudan a reducir la ansiedad y aumentar el enfoque", precio: 1980, talle: "M", img: './img/buzo.jpg'},
+
+    {id: 5, nombre: "sensorial 5", tipo: "tabla de equilibrio", cantidad: 1, desc: "tabla-balancin montessori para practicar equilibrio y destreza", precio: 1200, talle: "S", img: './img/buzo.jpg'},
+
+    {id: 6, nombre: "sensorial 6", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1500, talle: "S", img: './img/buzo.jpg'},
+   
+    
+]
